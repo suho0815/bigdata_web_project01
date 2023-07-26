89b3e4134c4579032d4b752f0c37b255298bb0db
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-07-26 18:03:45 +0900

    API 작성???

---
+++ pethospital/src/main/java/com/pethospital/domain/Pet_hospital.java
@@ -0,0 +1,33 @@
+package com.pethospital.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+@Getter
+@Entity
+public class Pet_hospital{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String phone_number;
+    private String street_address;
+    private String hospital_name;
+    private long longitude;
+    private long latitude;
+    private String province;   // 도/시
+    private String city;       // 시/군/구
+    private String detailcity;// 읍/면/동
+}
