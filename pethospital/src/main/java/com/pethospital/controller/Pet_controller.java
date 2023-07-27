d09da2a3879f914bb58e9ea561229eb0092f89bc
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-07-27 18:06:24 +0900

    서버연결이 안된다...

--- pethospital/src/main/java/com/pethospital/controller/Pet_controller.java
+++ pethospital/src/main/java/com/pethospital/controller/Pet_controller.java
@@ -4,14 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pethospital.service.Pet_hospital_Service;
-
-
 
 @RestController
 public class Pet_controller {
